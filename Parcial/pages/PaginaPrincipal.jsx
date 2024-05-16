f93b6a3b86ead95a3b4d518c28bbce785fca3022
Chorchhh <127800859+Chorchhh@users.noreply.github.com>
2024-05-15 21:31:30 -0300

    Update

--- Parcial/pages/PaginaPrincipal.jsx
+++ Parcial/pages/PaginaPrincipal.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import SportCard from "../components/Card";
+import Card from "../components/Card";
 
 const Info = () => {
   const [game, setGame] = useState([]);
